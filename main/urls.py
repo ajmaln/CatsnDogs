20065988c72c7dbec9c83aa8305eecd6e28e4572
Ajmal Noushad <ajufhn@gmail.com>
2017-12-22 01:36:58 +0530

    initial

--- main/urls.py
+++ main/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from main import views
 
 urlpatterns = [
+    path('/', views.index, name='index'),
     path('api/cats/<user>/<int:pk>', views.cat_list, name='cat_list'),
     path('api/dogs/<user>/<int:pk>', views.dog_list, name='dog_list'),
     path('api/cats/<user>', views.cat_list, name='cat_list'),
